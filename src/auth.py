fecf27d8fcb834471111cb034018f8294420bf79
franckbens <thisizbens@gmail.com>
2026-05-02 15:13:37 +0200

    Refactor project structure and improve repository organization

--- src/auth.py
+++ src/auth.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
 
 load_dotenv()
 
@@ -12,12 +13,19 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope="user-library-read playlist-read-private user-top-read"
 ))
 
-user = sp.current_user()
+results = sp.current_user_top_tracks(limit=20)
 
-print("Connecté à :", user["display_name"])
-print("\n🎵 Tes Top Tracks :\n")
-
-results = sp.current_user_top_tracks(limit=10)
+tracks = []
 
 for track in results["items"]:
-    print(track["name"], "-", track["artists"][0]["name"])
+    tracks.append({
+        "track_name": track["name"],
+        "artist": track["artists"][0]["name"],
+        "popularity": track["popularity"]
+    })
+
+df = pd.DataFrame(tracks)
+
+df.to_csv("data/top_tracks.csv", index=False)
+
+print("CSV créé : data/top_tracks.csv")
