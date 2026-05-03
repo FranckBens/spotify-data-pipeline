bdf6f6fc3183ebe1b0550a07bf6eb47a4ebebd3e
franckbens <thisizbens@gmail.com>
2026-05-03 05:11:35 +0200

    Implement PostgreSQL analytics and improve Spotify ETL pipeline

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
@@ -10,22 +10,37 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=os.getenv("CLIENT_ID"),
     client_secret=os.getenv("CLIENT_SECRET"),
     redirect_uri=os.getenv("REDIRECT_URI"),
-    scope="user-library-read playlist-read-private user-top-read"
+    scope="user-top-read"
 ))
 
 results = sp.current_user_top_tracks(limit=20)
 
 tracks = []
 
-for track in results["items"]:
+for item in results["items"]:
+
     tracks.append({
-        "track_name": track["name"],
-        "artist": track["artists"][0]["name"],
-        "popularity": track.get("popularity", None)
+        "track_id": item["id"],
+        "track_name": item["name"],
+        "main_artist": item["artists"][0]["name"],
+        "artist_count": len(item["artists"]),
+        "duration_ms": item["duration_ms"],
+        "explicit": item["explicit"],
+        "track_number": item["track_number"],
+        "disc_number": item["disc_number"],
+        "album_name": item["album"]["name"],
+        "album_type": item["album"]["album_type"],
+        "release_date": item["album"]["release_date"],
+        "album_total_tracks": item["album"]["total_tracks"],
+        "is_playable": item["is_playable"],
+        "is_local": item["is_local"],
+        "isrc": item["external_ids"]["isrc"],
+        "spotify_url": item["external_urls"]["spotify"]
     })
 
 df = pd.DataFrame(tracks)
 
 df.to_csv("data/top_tracks.csv", index=False)
 
-print("CSV créé : data/top_tracks.csv")
+print("CSV créé !")
+print(df.head())
--- src/load_to_postgres.py
+++ src/load_to_postgres.py
@@ -1,14 +1,8 @@
 import pandas as pd
 import psycopg2
 
-# Charger CSV
 df = pd.read_csv("data/top_tracks.csv")
 
-# Nettoyage popularity
-df["popularity"] = pd.to_numeric(df["popularity"], errors="coerce")
-df["popularity"] = df["popularity"].fillna(0).astype(int)
-
-# Connexion PostgreSQL
 conn = psycopg2.connect(
     dbname="spotify_db",
     user="postgres",
@@ -21,17 +15,18 @@ cur = conn.cursor()
 
 for _, row in df.iterrows():
     cur.execute("""
-        INSERT INTO top_tracks (track_name, artist, popularity)
-        VALUES (%s, %s, %s)
-    """, (
-        row["track_name"],
-        row["artist"],
-        row["popularity"]
-    ))
+        INSERT INTO top_tracks (
+            track_id, track_name, main_artist, artist_count,
+            duration_ms, explicit, track_number, disc_number,
+            album_name, album_type, release_date, album_total_tracks,
+            is_playable, is_local, isrc, spotify_url
+        )
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        ON CONFLICT (track_id) DO NOTHING
+    """, tuple(row))
 
 conn.commit()
-
 cur.close()
 conn.close()
 
-print("Données importées dans PostgreSQL ✅")
+print("Import PostgreSQL réussi")
